7bfb415e7b4d9417eccde945115cf16db4242f3c
DevMasterhack <67106437+JuanCanela@users.noreply.github.com>
2023-02-22 15:58:19 -0600

    Actualizacion a vite-config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://github.com/JuanCanela/apple--products-react",
+  base:"https://juancanela.github.io/apple--products-react/",
   plugins: [react()],
 })
